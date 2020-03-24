73620ba2e90ae691bc60183a1a2f10c5f011c1bb
Ashley Etemadi <ash.etemadi@gmail.com>
2020-03-24 02:58:11 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,7 +11,7 @@ end
 def print_first_directors_movie_titles
   index = 0 
   while index < directors_database.length do 
-    puts directors_database[index]
+    puts directors_database[index]\n 
     
     index += 1
   end 
